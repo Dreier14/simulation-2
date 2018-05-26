8c78b7e0db4d2d8921ad8190ffab29fee6054e51
Dreier14 <37100632+Dreier14@users.noreply.github.com>
2018-05-25 17:45:21 -0700

    "Completed for now"

---
+++ houser/src/App.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className = "box1">
+          <div className = "box2">  
+            
+I could stick around and get along with you, hello.
+It doesn't really mean that I'm into you, hello.
+You're alright but I'm here, darling, to enjoy the party.
+Don't get too excited 'cause that's all you get from me, hey.
+Yeah I think you're cute, but really you should know.
+I just came to say <div id = "box3">
+<h1><a href = "https://www.google.com/">GOOGLE</a></h1>
+<ol><li> Hello</li>
+<li> Goodbye</li>
+<li>Farewell</li> </ol>
+  <img src= "https://www.roseindia.net/tutorialfiles/26925.Span_TagPick.gif"/>
+         <h2><p> Hello </p></h2>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ houser/src/Component/Wizard/Wizard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Wizard extends Component {
+    constructor() {
+      super();
+    }
+
+
+    render(){
+        return(
+            <div> Dashboard </div>
+        )
+    }
+}
+
+export default Wizard;
